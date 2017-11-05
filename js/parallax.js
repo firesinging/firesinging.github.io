ba056fabc25e0dbf496be05516ee1daf62ab9573
FireSinger <31483091+FireSinger2000@users.noreply.github.com>
2017-11-05 20:31:02 +0100

    Update parallax.js

--- js/parallax.js
+++ js/parallax.js
@@ -1,17 +1,23 @@
-//  Scrolling
-
-UIkit.$doc.on('scrolling.uk.document', (function(){
-
-  var wrapper = $('.tm-sidebar-parallax .tm-sidebar-wrapper').css('background-position', (UIkit.langdirection == 'right' ? '100%':'0%') +' 0px');
-
-  return function() {
-
-    if(window.innerWidth <= 1024) return;
-
-    var x = UIkit.$win.scrollTop();
-
-    wrapper.css('background-position', (UIkit.langdirection == 'right' ? '100%':'0%') +' '+ parseInt(-x / 4) + 'px');
+(function($){
   
-  };
+  $(function(){
 
-})());
+    UIkit.$doc.on('scrolling.uk.document', (function(){
+
+      var wrapper = $('.tm-sidebar-parallax .tm-sidebar-wrapper').css('background-position', (UIkit.langdirection == 'right' ? '100%':'0%') +' 0px');
+
+      return function() {
+
+        if(window.innerWidth <= 1024) return;
+
+        var x = UIkit.$win.scrollTop();
+
+        wrapper.css('background-position', (UIkit.langdirection == 'right' ? '100%':'0%') +' '+ parseInt(-x / 4) + 'px');
+
+      };
+    
+    })());
+    
+  });
+
+})(jQuery);
